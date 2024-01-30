9c70f4b99a63d25d716f78992dabb538cd5b0757
Wijayaac <wijayaac25@gmail.com>
2024-01-31 03:52:48 +0800

    Update : CTA, About, banner section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import Image from "next/image"
-import styles from "./page.module.css"
-import Banner from "@/components/Banner"
+import Image from "next/image";
+import styles from "./page.module.css";
+import Banner from "@/components/Banner";
+import AboutUs from "@/components/AboutUs";
+import CallToAction from "@/components/CallToAction";
 
 export default function Home() {
-	return (
-		<main className={styles.main}>
-			<Banner />
-		</main>
-	)
+  return (
+    <main className={styles.main}>
+      <Banner />
+      <AboutUs />
+      <CallToAction />
+    </main>
+  );
 }
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -2,17 +2,19 @@ import React from 'react'
 
 import styles from './Banner.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 function Banner() {
 	return <div className={styles.banner}>
 		<Image src="/banner.jpg" alt="Banner" width={1920} height={1080} className={styles['banner-image']} />
-		<div className={styles['banner-wrapper']}>
+		<div className={`${styles['banner-wrapper']} section`}>
 
 			<div className={styles['banner-content']}>
 				<h1 className={styles['banner-title']}>
 					The inspired design
 					<br />of tomorrow</h1>
 				<p className={styles['banner-description']}>The innovative designs to enhance the human experience</p>
+				<a className={styles['banner-cta']} href="#portfolio">Lihat Portfolio</a>
 			</div>
 		</div>
 	</div>
