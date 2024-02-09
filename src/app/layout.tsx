0ccae5c38e70d51a7fae65f8331f7eaa5de96af7
Wijayaac <wijayaac25@gmail.com>
2024-02-10 07:07:51 +0800

    Add : google analytics & change email recipient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 const poppins = Poppins({ weight: ["400", "500", "600"], subsets: ["latin"] });
 // TODO: Add Google analytics
@@ -24,6 +25,16 @@ export default function RootLayout({
     <html lang='en'>
       <head>
         <meta charSet='UTF-8' />
+        <Script async src='https://www.googletagmanager.com/gtag/js?id=G-DC08J4MTK5' />
+
+        <Script id='google-analytics'>
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `}
+        </Script>
       </head>
       <body className={poppins.className}>
         <Header />
